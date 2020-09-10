e9d7485d86f902030cf269df440a263a9dad3a4a
ottos <Samuel.Otto@br-automation.com>
2020-09-10 15:56:42 +0200

    Updated mappServices to offical 5.11.2, Updated VSS112Q22.041P-000 to V 1.1.0.284

--- Logical/Libraries/MpCom/SG4/MpCom.h
+++ Logical/Libraries/MpCom/SG4/MpCom.h
@@ -1,6 +1,6 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
-/* MpCom 5.11.1 */
+/* MpCom 5.11.2 */
 
 #ifndef _MPCOM_
 #define _MPCOM_
@@ -9,7 +9,7 @@ extern "C"
 {
 #endif
 #ifndef _MpCom_VERSION
-#define _MpCom_VERSION 5.11.1
+#define _MpCom_VERSION 5.11.2
 #endif
 
 #include <bur/plctypes.h>
